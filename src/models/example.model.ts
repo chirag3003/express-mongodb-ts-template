e59f50b8d63adc2a2005271bb400bfc13049b9a7
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-07-27 00:22:15 +0530

    setup example model

---
+++ src/models/example.model.ts
@@ -0,0 +1,10 @@
+import mongoose from "mongoose";
+
+const exampleSchema = new mongoose.Schema(
+  {
+    msg: String,
+  },
+  { timestamps: true }
+);
+
+export const Example =  mongoose.model("example", exampleSchema);
