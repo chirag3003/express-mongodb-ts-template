eb22ceb3e6060764c2d1d52ff8d5051a495c5f06
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-07-27 12:37:54 +0530

    added comments

--- src/utils/db.ts
+++ src/utils/db.ts
@@ -5,8 +5,11 @@ const MONGODB_URI = process.env.MONGODB_URI
 const connectDB = async () => {
   try {
     await mongoose.connect(MONGODB_URI)
+
+    //checking if the database connection was successful
     if (mongoose.connection.readyState === 1)
       console.log('✅ MongoDB connected!')
+    //throwing an error if the connection was not successful
     else throw new Error('❌ MongoDB connection failed!')
   } catch (error) {
     console.log(error)
