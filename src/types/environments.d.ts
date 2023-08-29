47c0ad27176d2e30018d6a5fe3b7dc21c54caffe
Chirag Bhalotia <chiragbhalotia0412@gmail.com>
2023-08-30 00:21:23 +0530

    updated environment types

--- src/types/environments.d.ts
+++ src/types/environments.d.ts
@@ -4,6 +4,14 @@ declare global {
       PORT: string
       MONGODB_URI: string
       JWT_SECRET: string
+      ADMIN_JWT_SECRET: string
+      S3_ACCESS_KEY: string
+      S3_SECRET_KEY: string
+      S3_REGION: string
+      S3_BUCKET: string
+      S3_ENDPOINT: string
+      S3_OBJECT_URL: string
+      S3_OBJECT_FOLDER: string
     }
   }
 }
